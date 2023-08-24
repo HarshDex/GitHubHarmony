aeeaeb3b39a946f9b612f5b88fe21470572032c8
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2023-08-24 08:11:16 +0530

    added next div

--- miranda.js
+++ miranda.js
@@ -1,22 +1,22 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector("#main"),
+//   smooth: true,
+// });
 
-var tl = gsap.timeline()
-tl.to("#page-1",{
-    y : "100vh",
-    scale : ".5",
-    duration : 0    
-})
-tl.to("#page-1",{
-    y : "0vh",
-    duration : 1,
-    delay : .3
-})
-tl.to("#page-1",{
-    y : "0vh",
-    rotate : 360,
-    scale : 1,
-    duration : 1
-})
+// var tl = gsap.timeline();
+// tl.to("#page-1", {
+//   y: "100vh",
+//   scale: ".5",
+//   duration: 0,
+// });
+// tl.to("#page-1", {
+//   y: "0vh",
+//   duration: 1,
+//   delay: 0.3,
+// });
+// tl.to("#page-1", {
+//   y: "0vh",
+//   rotate: 360,
+//   scale: 1,
+//   duration: 1,
+// });
